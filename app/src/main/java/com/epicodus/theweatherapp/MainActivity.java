979bdf2317d0a98e11c02ab984661028f8735549
Kyle <kderrick1988@gmail.com>
2016-08-01 15:34:31 -0700

    add okhttp call for hardcoded long/lat

--- app/src/main/java/com/epicodus/theweatherapp/MainActivity.java
+++ app/src/main/java/com/epicodus/theweatherapp/MainActivity.java
@@ -2,8 +2,18 @@ package com.epicodus.theweatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +23,22 @@ public class MainActivity extends AppCompatActivity {
         String apiKey = "ab34d8a453d2fdfe605ed5b1cb5609bf";
         double latitude = 37.8267;
         double longittude = -122.423;
-        String forecastUrl = "https://api.forecast.io/forecast//,-122.423";
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +"/" + latitude + "," + longittude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            if(response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
 
     }
 }
