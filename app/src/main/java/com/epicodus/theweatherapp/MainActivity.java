480615b699882ecd6091f0290cb9bb86074742f0
Kyle <kderrick1988@gmail.com>
2016-08-01 18:56:18 -0700

    add getCurrentDetails to MainActivity

--- app/src/main/java/com/epicodus/theweatherapp/MainActivity.java
+++ app/src/main/java/com/epicodus/theweatherapp/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -20,6 +23,8 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +53,19 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
+                            mCurrentWeather = getCurrentDetails(jsonData);
 
                         } else {
                             alertUserAboutError();
                         }
-                    } catch (IOException e) {
+                    }
+                    catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                    catch (JSONException e) {
                         Log.e(TAG, "Exception caught: ", e);
                     }
                 }
@@ -64,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.networkUnavailable_message, Toast.LENGTH_LONG).show();
         }
         Log.d(TAG, "Main UI code is running");
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+        return new CurrentWeather();
     }
 
     private boolean isNetworkAvailable() {
