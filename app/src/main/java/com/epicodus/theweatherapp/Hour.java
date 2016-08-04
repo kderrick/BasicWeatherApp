c728f3d170c1876094186391761ce13a26e0e8ed
Kyle <kderrick1988@gmail.com>
2016-08-03 18:36:51 -0700

    create Hour data model class

---
+++ app/src/main/java/com/epicodus/theweatherapp/Hour.java
@@ -0,0 +1,50 @@
+package com.epicodus.theweatherapp;
+
+public class Hour {
+
+    private long mTime;
+    private String mSummary;
+    private double mTemperature;
+    private String mIcon;
+    private String mTimezone;
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long mTime) {
+        this.mTime = mTime;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double mTemperature) {
+        this.mTemperature = mTemperature;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String mIcon) {
+        this.mIcon = mIcon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String mTimezone) {
+        this.mTimezone = mTimezone;
+    }
+}
