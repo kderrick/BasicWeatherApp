1c1c45e0fb673b2259666a49104e5e14f507bfcb
Kyle <kderrick1988@gmail.com>
2016-08-03 18:42:46 -0700

    add Day data model

---
+++ app/src/main/java/com/epicodus/theweatherapp/Day.java
@@ -0,0 +1,49 @@
+package com.epicodus.theweatherapp;
+
+public class Day {
+    private long mTime;
+    private String mSummary;
+    private double mTemperatureMax;
+    private String mIcon;
+    private String mTimezone;
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long mTime) {
+        this.mTime = mTime;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String mSummary) {
+        this.mSummary = mSummary;
+    }
+
+    public double getTemperatureMax() {
+        return mTemperatureMax;
+    }
+
+    public void setTemperatureMax(double mTemperatureMax) {
+        this.mTemperatureMax = mTemperatureMax;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String mIcon) {
+        this.mIcon = mIcon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String mTimezone) {
+        this.mTimezone = mTimezone;
+    }
+}
