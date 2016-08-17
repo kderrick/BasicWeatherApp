5b0493a879ca6e25ed0244ef22c0ceb8ab39eb16
Kyle <kderrick1988@gmail.com>
2016-08-17 12:09:39 -0700

    add HourlyForecastActivity and make Hour model parcleable

---
+++ app/src/main/java/com/epicodus/theweatherapp/UI/HourlyForecastActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.theweatherapp.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.theweatherapp.R;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+    }
+}
