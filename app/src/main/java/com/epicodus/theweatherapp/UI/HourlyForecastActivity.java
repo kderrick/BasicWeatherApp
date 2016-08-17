71d010b2b18e1ec33e3707653deb0722a90debae
Kyle <kderrick1988@gmail.com>
2016-08-17 12:17:51 -0700

    make HourlyForecastActivity accept parcel

--- app/src/main/java/com/epicodus/theweatherapp/UI/HourlyForecastActivity.java
+++ app/src/main/java/com/epicodus/theweatherapp/UI/HourlyForecastActivity.java
@@ -1,15 +1,26 @@
 package com.epicodus.theweatherapp.UI;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.theweatherapp.R;
+import com.epicodus.theweatherapp.weather.Hour;
+
+import java.util.Arrays;
 
 public class HourlyForecastActivity extends AppCompatActivity {
+
+    private Hour[] mHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
     }
 }
